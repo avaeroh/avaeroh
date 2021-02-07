4364eb3694724b88c10aef9285aa004c1f0ae717
Jon <jonathan.mh@hotmail.co.uk>
2021-02-07 16:43:41 +0000

    added console logging for server details

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const status = require('minecraft-server-status');
+
+var info;
+
+status('avaeroh.hopto.org', 25565, response => {
+info=JSON.stringify(response);
+console.log(info);
+})
 
 class App extends Component {
   render() {
@@ -8,11 +16,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+          <h2>Avaeroh's Webapp</h2>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        {info}
         </p>
+       </div>
+       <div>{console.log(info)}</div>
       </div>
     );
   }
